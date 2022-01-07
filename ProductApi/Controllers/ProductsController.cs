4f7fdf7e13687f20a48134928d87e31214f796bd
tylerleesanti <tylerleesanti@gmail.com>
2022-01-07 17:00:41 -0600

    First comit

---
+++ ProductApi/Controllers/ProductsController.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ProductApi.Daos;
+using ProductApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProductApi.Controllers
+{
+    [ApiController]
+    [ApiVersion("1.0")]
+    [Route("v{version:apiVersion}/[controller]")]
+    [Produces("application/json")]
+    public class ProductsController : ControllerBase
+    {
+        private readonly ProductContext _context;
+
+        public ProductsController(ProductContext context)
+        {
+            _context = context;
+
+            if (_context.Products.Any()) return;
+
+            ProductSeed.InitData(context);
+        }
+
+        [HttpGet]
+        [Route("{productNumber}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public ActionResult<Product> GetProductByProductNumber([FromRoute]
+            string productNumber)
+        {
+            var productDb = _context.Products
+              .FirstOrDefault(p => p.ProductNumber.Equals(productNumber,
+                        StringComparison.InvariantCultureIgnoreCase));
+
+            if (productDb == null) return NotFound();
+
+            return Ok(productDb);
+        }
+
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public ActionResult<Product> PostProduct([FromBody] Product product)
+        {
+            try
+            {
+                _context.Products.Add(product);
+                _context.SaveChanges();
+
+                return new CreatedResult($"/products/{product.ProductNumber.ToLower()}", product);
+            }
+            catch (Exception e)
+            {
+                // Typically an error log is produced here
+                return ValidationProblem(e.Message);
+            }
+        }
+    }
+}
